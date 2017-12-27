68cac70952cd43d6bfaa636b122cb644eab0525d
Ryan Williams <ryan.williams@projectpeople.com>
2017-12-27 14:16:57 +0000

    Moved some bits, added the beginnings of the login form.

--- src/app/components/functions/login/login.component.ts
+++ src/app/components/functions/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  @ViewChild('usernameInput') nameInputRef: ElementRef;
+  @ViewChild('passwordInput') passwordInputRef: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  doLogIn() {
+    const username = this.nameInputRef.nativeElement.value;
+    const password = this.passwordInputRef.nativeElement.value;
+    console.log('name is ', username, ' and password is ', password);
+    //TODO: get token etc for logged in user
   }
 
 }
